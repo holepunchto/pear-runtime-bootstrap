183ec93f1e7e750de124e3a649022fc68630aa6f
Mathias Buus <mathiasbuus@gmail.com>
2024-10-02 15:23:52 +0200

    scope tar to os/arch

--- pack.js
+++ pack.js
@@ -4,9 +4,14 @@ import tar from 'tar-fs'
 import zlib from 'zlib'
 import fs from 'fs'
 
-tar.pack('build')
+tar.pack('build', { map })
   .pipe(zlib.createGzip())
   .pipe(fs.createWriteStream('build.tar.gz'))
   .on('finish', function () {
     console.log('Done! wrote to', 'build.tar.gz')
   })
+
+function map (header) {
+  header.name = 'by-arch/' + process.platform + '-' + process.arch + '/' + header.name
+  return header
+}
